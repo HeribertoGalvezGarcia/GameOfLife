e9b34dc82a8e33a921321e0a4004c12000998348
HeribertoGalvezGarcia <heriberto.galvezgarcia@gmail.com>
2020-06-26 08:13:10 -0700

    Add rules

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
     <>
       <Controller/>
       <Grid />
+
+      Rules: If a cell is alive and it has two or three alive neighbors it continues living. Otherwise it dies. If a cell is dead and it has three neighbors it becomes alive.
     </>
   );
 }
