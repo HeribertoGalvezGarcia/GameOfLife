842fbe0bba2eb5a32a9f9baf9fd1597eb1269717
HeribertoGalvezGarcia <heriberto.galvezgarcia@gmail.com>
2020-06-26 09:55:11 -0700

    Seperate rules from rest

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Controller/>
       <Grid />
 
-      Rules: If a cell is alive and it has two or three alive neighbors it continues living. Otherwise it dies. If a cell is dead and it has three neighbors it becomes alive.
+      <p style={{border: "1px solid black", margin: "20px auto", textAlign: "center", width: "60%"}}>Rules: If a cell is alive and it has two or three alive neighbors it continues living. Otherwise it dies. If a cell is dead and it has three neighbors it becomes alive.</p>
     </>
   );
 }
