56ea23f4d4a449f8ceea79cc3cccbcb916ec9859
HeribertoGalvezGarcia <heriberto.galvezgarcia@gmail.com>
2020-06-26 09:31:26 -0700

    Updates

---
+++ precache-manifest.f050aa7a9b49e1fa423765e1cb65f30c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e02b7e33769819a15739cdedb1b6e10",
+    "url": "/HeribertoGalvezGarcia/GameOfLife/index.html"
+  },
+  {
+    "revision": "88ddf8bf95bce5e7ecd0",
+    "url": "/HeribertoGalvezGarcia/GameOfLife/static/js/2.81d7e6a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HeribertoGalvezGarcia/GameOfLife/static/js/2.81d7e6a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4f2787b6d72ef2b719c",
+    "url": "/HeribertoGalvezGarcia/GameOfLife/static/js/main.0b211bfa.chunk.js"
+  },
+  {
+    "revision": "52307890bc00eb0fed38",
+    "url": "/HeribertoGalvezGarcia/GameOfLife/static/js/runtime-main.676c45a2.js"
+  }
+]);
